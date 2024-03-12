d28680750993ecadfaca10faba2ea0b6006cee92
KN-sunil <sunilsun837@gmail.com>
2024-03-12 21:46:46 +0530

    EB

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 //import Product from "./Product/Product1"
-import Product2 from "./Product/Product2"
+/* import Product2 from "./Product/Product2" */
 import Navbar from "./Navbar"
+import Message from "./EB/Message"
 function App() {
   return (
     <div>
       <Navbar/>
-       <Product/>
+       <Message/>
     </div>
   )
 }
