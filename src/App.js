897f842946bbec804a1c2b01933a0b70d09fd9e7
KN-sunil <sunilsun837@gmail.com>
2024-03-20 12:32:41 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 //import Message from "./Hooks/Messagepv";
 import Message from "./Hooks/Messagenpv";
-import Counter from "./Hooks/Counter";
+// import Counter from "./Hooks/Counter";
 
 class App extends Component{
   render(){
     return <div>
       <h1>App component</h1>
-      <Counter/>
+      <Message/>
     </div>
   }
 }
--- src/Hooks/Messagenpv.jsx
+++ src/Hooks/Messagenpv.jsx
@@ -1,29 +1,32 @@
 
+import { computeHeadingLevel } from "@testing-library/react";
 import React,{useState} from "react";
 
 let Message=()=>{
     let[state,setState]=useState({Message:"hello"})
-
+    console.log(state)
     let gmHandler=()=>{
         setState({Message:"Good morning"})
+        console.log("updated")
     }
     let gaHandler=()=>{
         setState({Message:"Good afternoon"})
     }
-    // let geHandler=()=>{
-    //     setState({Message:"Good evening"})
-    // }
-    // let gnHandler=()=>{
-    //     setState({Message:"Good night"})
-    // }
+     let geHandler=()=>{
+         setState({Message:"Good evening"})
+
+    }
+     let gnHandler=()=>{
+        setState({Message:"Good night"})
+     }
     return (
         <div>
             <h1>Message component</h1>
             <h2>Message:{state.Message}</h2>
             <button onClick={gmHandler}>GM</button>
             <button onClick={gaHandler}>GA</button>
-            {/* <button onClick={()=>{setState.Message("Good evening")}}>GE</button>
-            <button onClick={()=>{setState.Message("Good night")}}>GN</button> */}
+            <button onClick={geHandler}>GE</button>
+            <button onClick={gaHandler}>GN</button> 
         </div>
     )
 }
