fdff57a6537fff4648be70ef3e5be2426ca9b0db
KN-sunil <sunilsun837@gmail.com>
2024-03-28 15:06:36 +0530

    .

---
+++ backupreduxproduct/src/redux/store.js
@@ -0,0 +1,7 @@
+import { createStore } from "redux";
+import {productReducer} from "./product/product.reducer";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
+let store=createStore(productReducer,composeWithDevTools())
+
+export {store}
