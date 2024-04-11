09408742ca7021aadd4bffa5e2af51d61b059097
KN-sunil <sunilsun837@gmail.com>
2024-04-11 15:22:17 +0530

    .

---
+++ backupuseref/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Fragment } from 'react'
+import Reg from  "./Hooks/Reg"
+
+const App = () => {
+  return (
+   <Fragment>
+    <nav>
+      <a href='#'>Use ref example</a>
+    </nav>
+    <Reg/>
+   </Fragment>
+  )
+}
+
+export default App
--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import { Fragment } from 'react'
-import Reg from  "./Hooks/Reg"
+import React, { Fragment } from 'react'
+import {Link,BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import Products from './products/Products'
+import Admin from './products/Admin'
+import CreateProduct from './products/CreateProduct'
 
 const App = () => {
-  return (
-   <Fragment>
-    <nav>
-      <a href='#'>Use ref example</a>
-    </nav>
-    <Reg/>
-   </Fragment>
-  )
+ return <Fragment>
+  <Router>
+        <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
+        <Link to="/" className='navbar-brand'>Product Cart</Link>
+        <div className='ml-auto'>
+          <ul className='navbar-nav'>
+            <li><Link  className='nav-link' to="/products">Products</Link></li>
+            <li><Link className='nav-link' to="/create">New Product</Link></li>
+            <li><Link  className='nav-link' to="/admin">Admin</Link></li>
+          </ul>
+        </div>
+        </nav>
+        <Routes>
+        <Route path="/products" element={<Products/>} />
+        <Route path="/admin" element={<Admin/>} />
+        <Route path="/create" element={<CreateProduct/>} />
+        </Routes>
+  </Router>
+       </Fragment>
 }
 
 export default App
